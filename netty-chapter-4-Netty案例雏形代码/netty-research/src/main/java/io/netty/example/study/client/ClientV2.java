68bdb58808b9473d9276859cc599f3268517b8bd
worldluoji <qq1132416@outlook.com>
2021-01-15 17:25:47 +0800

    first commit

---
+++ netty-chapter-4-Netty案例雏形代码/netty-research/src/main/java/io/netty/example/study/client/ClientV2.java
@@ -0,0 +1,85 @@
+package io.netty.example.study.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioChannelOption;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.example.study.client.codec.*;
+import io.netty.example.study.client.handler.dispatcher.OperationResultFuture;
+import io.netty.example.study.client.handler.dispatcher.RequestPendingCenter;
+import io.netty.example.study.client.handler.dispatcher.ResponseDispatcherHandler;
+import io.netty.example.study.common.OperationResult;
+import io.netty.example.study.common.RequestMessage;
+import io.netty.example.study.common.order.OrderOperation;
+import io.netty.example.study.util.IdUtil;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.util.concurrent.ExecutionException;
+
+public class ClientV2 {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.channel(NioSocketChannel.class);
+        // 设置客户端连接超时时间为10秒
+        bootstrap.option(NioChannelOption.CONNECT_TIMEOUT_MILLIS, 10000);
+
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try{
+            bootstrap.group(group);
+            RequestPendingCenter requestPendingCenter = new RequestPendingCenter();
+
+            bootstrap.handler(new ChannelInitializer<NioSocketChannel>() {
+                @Override
+                protected void initChannel(NioSocketChannel ch) throws Exception {
+                    ChannelPipeline pipeline = ch.pipeline();
+                    pipeline.addLast(new OrderFrameDecoder());
+                    pipeline.addLast(new OrderFrameEncoder());
+
+                    pipeline.addLast(new OrderProtocolEncoder());
+                    pipeline.addLast(new OrderProtocolDecoder());
+
+                    pipeline.addLast(new ResponseDispatcherHandler(requestPendingCenter));
+
+                    pipeline.addLast(new OperationToRequestMessageEncoder());
+
+                    pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+                }
+            });
+
+            // 连接服务器，但是因为connect方法时异步的，所以下面需要sync()阻塞
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 8090);
+
+            channelFuture.sync();
+
+            long streamId = IdUtil.nextId();
+
+            RequestMessage requestMessage = new RequestMessage(
+                    streamId, new OrderOperation(1001, "tudou"));
+
+            OperationResultFuture operationResultFuture = new OperationResultFuture();
+
+            requestPendingCenter.add(streamId, operationResultFuture);
+
+            channelFuture.channel().writeAndFlush(requestMessage);
+
+            // 阻塞，直到上面ResponseDispatcherHandler返回结果，设置返回的东东
+            OperationResult operationResult = operationResultFuture.get();
+
+            System.out.println(operationResult);
+
+            // 客户端同步断开链接时，这句才会往下执行。让线程进入wait状态，也就是main线程不会执行到finally里面，
+            // client持续运行, 直到监听到关闭事件
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            System.out.println("client v2 down...");
+            group.shutdownGracefully();
+        }
+    }
+
+}
---
+++ netty-chapter-4-Netty案例雏形代码/netty-research/src/main/java/io/netty/example/study/server/Server.java
@@ -0,0 +1,70 @@
+package io.netty.example.study.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioChannelOption;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.example.study.server.codec.OrderFrameDecoder;
+import io.netty.example.study.server.codec.OrderFrameEncoder;
+import io.netty.example.study.server.codec.OrderProtocolDecoder;
+import io.netty.example.study.server.codec.OrderProtocolEncoder;
+import io.netty.example.study.server.handler.OrderServerProcessHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
+
+import java.util.concurrent.ExecutionException;
+
+public class Server {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.channel(NioServerSocketChannel.class);
+
+        serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
+
+        // 关闭negal算法，即小包可立即发出，不必等待
+        serverBootstrap.childOption(NioChannelOption.TCP_NODELAY, true);
+        // 设置等待队列中的连接数（当连接不够用时，后来的链接会放到队列中等待）
+        serverBootstrap.option(NioChannelOption.SO_BACKLOG, 1024);
+
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1, new DefaultThreadFactory("boss"));
+        NioEventLoopGroup group = new NioEventLoopGroup(2, new DefaultThreadFactory("worker"));
+
+        try{
+            serverBootstrap.group(bossGroup, group);
+
+            serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
+                @Override
+                protected void initChannel(NioSocketChannel ch) throws Exception {
+                    ChannelPipeline pipeline = ch.pipeline();
+
+                    pipeline.addLast("frameDecoder", new OrderFrameDecoder());
+                    pipeline.addLast("frameEncoder", new OrderFrameEncoder());
+
+                    pipeline.addLast("protocolEncoder", new OrderProtocolEncoder());
+                    pipeline.addLast("protocolDecoder", new OrderProtocolDecoder());
+
+                    pipeline.addLast("logger", new LoggingHandler(LogLevel.INFO));
+
+                    pipeline.addLast("orderHandler", new OrderServerProcessHandler());
+                }
+            });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8090).sync();
+            // 阻塞，直到监听到关闭事件
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            System.out.println("Server down...");
+            group.shutdownGracefully();
+        }
+
+
+    }
+
+}
