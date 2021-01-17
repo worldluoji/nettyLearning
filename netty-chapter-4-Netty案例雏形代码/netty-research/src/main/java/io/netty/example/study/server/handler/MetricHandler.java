bc05840dbd0e6f99857e1b5cd17b3930638c7dbc
worldluoji <qq1132416@outlook.com>
2021-01-17 13:09:17 +0800

    @统计示例@luoji@20210117

---
+++ netty-chapter-4-Netty案例雏形代码/netty-research/src/main/java/io/netty/example/study/server/handler/MetricHandler.java
@@ -0,0 +1,27 @@
+package io.netty.example.study.server.handler;
+
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+// ChannelDuplexHandler既支持读也支持写
+public class MetricHandler extends ChannelDuplexHandler {
+
+    // 可将该数据发送到elk存储和展示
+    private AtomicLong totalConnections = new AtomicLong();
+
+    // 表示链接建立时执行
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        totalConnections.incrementAndGet();
+        super.channelActive(ctx);
+    }
+
+    // 表示链接释放时执行
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        totalConnections.decrementAndGet();
+        super.channelInactive(ctx);
+    }
+}
